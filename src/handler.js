2d9a2b2be365a588389c6b9be26ba738f1f56118
kadhit <92635388+kadhit@users.noreply.github.com>
2022-06-16 20:10:05 +0700

    saving notes

---
+++ src/handler.js
@@ -0,0 +1,38 @@
+const { nanoid } = import('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (req, h) => {
+  const { title, tags, body } = req.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const res = h.response({
+      status: 'success',
+      message: 'Note successfully added.',
+      data: {
+        noteId: id,
+      },
+    });
+    res.code(201);
+    return res;
+  }
+
+  const res = h.response({
+    status: 'fail',
+    message: 'Note cannot be added.',
+  });
+  res.code(501);
+  return res;
+};
+
+module.exports = { addNoteHandler };
